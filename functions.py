3c04116b692a6e9e4992276fe007512389d0144b
Mariana Velandia <marisavelan@gmail.com>
2023-07-28 08:24:49 -0500

    se agregaron 15 lineas de codigo al arcivo funtions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,19 @@
 def calculate_payment(hours_worked, hourly_rate):
-    pass
+   if hours_worked < 0 or hourly_rate < 0:
+        return -1
+    elif hours_worked > 40:
+        overtime_hours = hours_worked - 40
+        hours_worked = 40
+    else:
+        overtime_hours = 0
 
+    regular_payment = hours_worked * hourly_rate
+    overtime_payment = overtime_hours * (hourly_rate * 1.5)
+    total_payment = regular_payment + overtime_payment
+    social_benefits_discount = total_payment * 0.06
+    total_payment -= social_benefits_discount
+
+    return total_payment
 def compare_numbers(num1, num2):
     pass
     
